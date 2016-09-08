9893350abba03a7dca9d5368dd3a0df70f9daa02
frombelair <lbenchouk@gmail.com>
2016-09-08 13:23:59 -0400

    home pratice arrays

--- array/array_function.rb
+++ array/array_function.rb
@@ -1,41 +1,19 @@
 def array_mined_minds
-    
-    my_array = [*1..100]   # start an array called my_array  
-    my_array.map! do |number| # putting the number into the array( [1,2,3,4...100] )
-    	if number % 15 == 0 
-    		"mined minds"
-   		elsif 
-   			number % 3 == 0
-    		"mined"
-		elsif 
-			number % 5 == 0
-			"minds"
-		else
-			number
-		end
-	end 
-    my_array
-   
+  my_array=[*1..100]
+  my_array.map! do |number|
+    if number % 15 == 0
+        "minedminds"
+    elsif 
+        number % 3 == 0
+        "mined"
+    elsif 
+        number % 5 == 0
+        "minds"
+    else 
+        number
+    end
+  end
+  my_array
 end
-
-
-
-# def array_mined_minds
-    
-#     my_array = []
-#     (1..100).each do |number|
-    
-#     if number % 15 == 0
-    
-#     number = "mined minds"
-#     elsif number % 3 == 0
-#     number = "mined"
-# elsif number % 5 == 0
-# 	number = "minds"
-# else
-# 	number = number
-# end
-# my_array << number
-# end
-# my_array 
-# end
+      
+      
--- array/test_array_function.rb
+++ array/test_array_function.rb
@@ -1,30 +1,24 @@
 require "minitest/autorun"
 require_relative "array_function.rb"
-class TestArrayFunction <Minitest::Test
-    
-   
+class TestArrayFunction<MiniTest::Test
 
     def test_first_equals_1
-        result = array_mined_minds()
-        assert_equal(1, result[0])
+        result=array_mined_minds()
+        assert_equal(1,result[0])
     end
-
+    
     def test_mined
-        result = array_mined_minds()
-        assert_equal("mined", result[2])
+        result=array_mined_minds()
+        assert_equal("mined",result[2])
     end
-
     def test_minds
-        result = array_mined_minds()
-        assert_equal("minds", result[4])
+        result=array_mined_minds()
+        assert_equal("minds",result[4])
     end
-
-    def test_Mined_Minds
-        result = array_mined_minds()
-        assert_equal("mined minds", result[14])
+    def test_mined_minds
+        result=array_mined_minds()
+        assert_equal("minedminds",result[14])
     end
-
-
 
 end
 
