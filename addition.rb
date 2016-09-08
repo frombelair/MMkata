92dbb75f1c49437021470da1512904f866d9e079
frombelair <lbenchouk@gmail.com>
2016-09-08 15:31:01 -0400

    learning ruby from youtube 9/8

--- addition.rb
+++ addition.rb
@@ -1,5 +1,12 @@
-def add (num1,num2)
-	puts num1+num2
-end  
+your_age = 21
+	if your_age <= 17
+	   puts "your are not old enough for this site!"
+	elsif your_age > 100
+		puts "go to this site: www.google.com"
+	elsif your_age == 21
+		puts"you just got updated to the premium version!"
+ 
 
-add(15, 20)
+	else
+		puts "what is your gender?"
+	end
