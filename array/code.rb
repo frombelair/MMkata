a4dcd7458eec4e7bd6cd01b9ae001aa2358138f2
frombelair <lbenchouk@gmail.com>
2016-09-13 11:18:03 -0400

    winning tickets tests class 9/13

--- array/code.rb
+++ array/code.rb
@@ -1,11 +1,18 @@
-def off_by_one(my_ticket, winning_tickets)
-	i = 0
-	c = 0
-	my_ticket.length.times do
-		if my_ticket[i] == winning_tickets[i]			
-			c += 1
-		end
-		i += 1
+# def off_by_one(my_ticket, winning_tickets)
+# 	i = 0
+# 	c = 0
+# 	my_ticket.length.times do
+# 		if my_ticket[i] == winning_tickets[i]			
+# 			c += 1
+# 		end
+# 		i += 1
+# 	end
+# 	c == my_ticket.length - 1 ? true : false
+# end
+def find_winners(our_ticket, winning_tickets)
+	matches = []
+	if winning_tickets.include?(our_ticket)
+	matches << our_ticket  
 	end
-	c == my_ticket.length - 1 ? true : false
+matches
 end
--- array/test_code.rb
+++ array/test_code.rb
@@ -3,27 +3,52 @@ require_relative "code.rb"
 
 class TestforwinnerFunction < Minitest::Test
 
-	def test_matching_ticket_returns_false
-        my_ticket = "4567"
-        winning_tickets = "4567"
-        assert_equal(false, off_by_one(my_ticket, winning_tickets))
-    end 
+# def test_matching_ticket_returns_false
+#         my_ticket = "4567"
+#         winning_tickets = "4567"
+#         assert_equal(false, off_by_one(my_ticket, winning_tickets))
+#     end 
 
-	def test_ticket_off_by_2_numbers_returns_false
-        my_ticket = "4567"
-        winning_tickets = "4589"
-        assert_equal(false, off_by_one(my_ticket, winning_tickets))
-    end
-    def test_ticket_off_by_1_returns_true
-        my_ticket = "4456"
-        winning_tickets = "5456"
-        assert_equal(true, off_by_one(my_ticket, winning_tickets))
-    end
-    def test_ticket_5_numbers_off_by_1_number_returns_true
-        my_ticket = "45674"
-        winning_tickets = "45675"
-        assert_equal(true, off_by_one(my_ticket, winning_tickets))
-    end
 
+# def test_ticket_off_by_2_numbers_returns_false
+#         my_ticket = "4567"
+#         winning_tickets = "4589"
+#         assert_equal(false, off_by_one(my_ticket, winning_tickets))
+#     end
+
+#  def test_ticket_off_by_1_returns_true
+#         my_ticket = "4456"
+#         winning_tickets = "5456"
+#         assert_equal(true, off_by_one(my_ticket, winning_tickets))
+#     end
+#  def test_ticket_5_numbers_off_by_1_number_returns_true
+#         my_ticket = "45674"
+#         winning_tickets = "45675"
+#         assert_equal(true, off_by_one(my_ticket, winning_tickets))
+#     end
+
+# end
+def test_one_equals_one
+assert_equal(1,1)
+end 
+
+
+def test_return_empty_array_when_no_winning_tickets
+our_tickets = "1234"
+winning_tickets = []
+assert_equal([], find_winners(our_tickets, winning_tickets))
+end
+
+def test_no_matches_returns_empty_array
+my_ticket = "1234"
+winning_tickets = ["6666", "7777", "9876"]
+assert_equal([], find_winners(my_ticket, winning_tickets))
+end 
+
+def test_return_array_with_ticket_with_single_match
+my_ticket = "1234"
+winning_tickets = ["1234"]
+assert_equal(["1234"], find_winners(my_ticket, winning_tickets))
+end 
 
 end
